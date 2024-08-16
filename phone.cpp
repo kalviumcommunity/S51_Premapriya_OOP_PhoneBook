8e73726d62cc269be4deb12f4ded2c0fe2352f16
Premapriya1905 <Premapriya1905@gmail.com>
2024-08-16 09:49:09 +0530

    new-delete

--- phone.cpp
+++ phone.cpp
@@ -19,7 +19,7 @@ public:
         this->address = address;
     }
 
-    void displayContact() {
+    void displayContact() const {
         cout << "Name: " << this->name << endl;
         cout << "Phone Number: " << this->phoneNumber << endl;
         cout << "Email: " << this->email << endl;
@@ -37,46 +37,45 @@ public:
 class Phonebook {
 public:
     // Member variables
-    vector<Contact> contacts;
+    vector<Contact* > contacts;
+
+    // Destructor to free dynamically allocated memory
+    Phonebook(){
+        for (Contact* contact:contacts){
+            delete contact;
+        }
+    }
 
     // Member functions
-    void addContact(Contact contact) {
+    void addContact(Contact* contact) {
         contacts.push_back(contact);
     }
 
     void displayAllContacts() {
-        for (Contact contact : contacts) {
-            contact.displayContact();
+        for (Contact* contact : contacts) {
+            contact->displayContact();
             cout << endl;
         }
     }
 };
 
 int main() {
-    // Array of Contact objects
-    Contact contactArray[2];
+    // Dynamically allocating memory for Contact objects
+    Contact* contact1 = new Contact("Prema", "123-456-7890", "prema@example.com", "123 Main St");
+    Contact* contact2 = new Contact("Priya", "987-654-3210", "priya@example.com", "456 Elm St");
 
-    // Populating array with contact information
-    contactArray[0].updateContact("Prema", "123-456-7890", "prema@example.com", "123 Main St");
-    contactArray[1].updateContact("Priya", "987-654-3210", "priya@example.com", "456 Elm St");
-
-    // Displaying contact details from array
-    cout << "\nContact Details from Array:" << endl;
-    for (int i = 0; i < 2; i++) {
-        contactArray[i].displayContact();
-        cout << endl;
-    }
-
+    contact1 -> displayContact();
+    cout<<endl;
+    contact2->displayContact();
+    cout<<endl;
+    
     // Creating Phonebook object
     Phonebook phonebook;
 
-    // Adding contacts to phonebook from array
-    for (int i = 0; i < 2; i++) {
-        phonebook.addContact(contactArray[i]);
-    }
+    phonebook.addContact(contact1);
+    phonebook.addContact(contact2);
 
     // Displaying all contacts in the phonebook
-    cout << "\nPhonebook Contacts:" << endl;
     phonebook.displayAllContacts();
 
     return 0;
